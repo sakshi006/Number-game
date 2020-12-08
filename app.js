ff795aff073f34a28aaf3a1eeba61774c3333995
Sakshi Kumar <Saks5011@gmail.com>
2020-12-09 01:13:01 +0530

    JS done

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
-//numbers
+var input = document.querySelector("#text");
+var btn = document.querySelector("#btn");
+var outputNumber = document.querySelector("#left");
+var outputRoman = document.querySelector("#right");
 
-"https://api.funtranslations.com/translate/"
+// btn.addEventListener("click", function(){
+//     console.log("hey");
+// })
 
+var serverURLn ="https://api.funtranslations.com/translate/numbers.json";
+var serverURLr ="https://api.funtranslations.com/translate/roman-numerals.json";
+function getTranslation1(input){
+    return serverURLn + "?" + "text=" + input
+  }
 
-//roman
-"https://api.funtranslations.com/translate/"
+function getTranslation2(input){
+    return serverURLr + "?" + "text=" + input
+  }
+function errorHandler(error){
+    console.log("Error Occured",error);
+    alert("Something is wrong with the server! Try again later")
+}
+function clickHandler(){
+    var inputText=input.value;
+    fetch(getTranslation1(inputText))
+        .then(response => response.json())
+        .then(json => {
+          var translatedText1 = json.contents.translated;
+          outputNumber.innerHTML=translatedText1; //output
+        })
+    fetch(getTranslation2(inputText))
+        .then(response => response.json())
+        .then(json => {
+          var translatedText2 = json.contents.translated;
+          outputRoman.innerHTML=translatedText2; //output
+        })
+        .catch(errorHandler)
+}
+
+btn.addEventListener("click", clickHandler);
