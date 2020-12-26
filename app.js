74f87ecc7dc6fa40f6e63ae95e8e1a39b9d0db8f
Vals <1884780+VallarasuS@users.noreply.github.com>
2020-12-26 21:58:24 +0530

    chore: minor refactor

--- app.js
+++ app.js
@@ -1,40 +1,32 @@
-var input = document.querySelector("#text");
-var btn = document.querySelector("#btn");
-var outputNumber = document.querySelector("#left");
-var outputRoman = document.querySelector("#right");
+const input = document.querySelector("#text");
+const btn = document.querySelector("#btn");
+const outputNumber = document.querySelector("#left");
+const outputRoman = document.querySelector("#right");
 
-// btn.addEventListener("click", function(){
-//     console.log("hey");
-// })
+const serverURLn = "https://api.funtranslations.com/translate/numbers.json?text=";
+const serverURLr = "https://api.funtranslations.com/translate/roman-numerals.json?text=";
 
-var serverURLn ="https://api.funtranslations.com/translate/numbers.json";
-var serverURLr ="https://api.funtranslations.com/translate/roman-numerals.json";
-function getTranslation1(input){
-    return serverURLn + "?" + "text=" + input
-  }
+function getTranslation(url, input) {
+    const query = url + input;
+    return fetch(query).then(response => response.json());
+}
 
-function getTranslation2(input){
-    return serverURLr + "?" + "text=" + input
-  }
-function errorHandler(error){
+function errorHandler(error) {
     console.log("Error Occured",error);
     alert("Something is wrong with the server! Try again later")
 }
-function clickHandler(){
-    var inputText=input.value;
-    fetch(getTranslation1(inputText))
-        .then(response => response.json())
-        .then(json => {
-          var translatedText1 = json.contents.translated;
-          outputNumber.innerHTML=translatedText1; //output
-        })
-    fetch(getTranslation2(inputText))
-        .then(response => response.json())
-        .then(json => {
-          var translatedText2 = json.contents.translated;
-          outputRoman.innerHTML=translatedText2; //output
-        })
-        .catch(errorHandler)
+
+function clickHandler() {
+    var inputText = input.value;
+    getTranslation(serverURLn, inputText).then(json => {
+        var translatedText1 = json.contents.translated;
+        outputNumber.innerHTML=translatedText1; //output
+    }).catch(errorHandler)
+    
+    getTranslation(serverURLr, inputText).then(json => {
+        var translatedText2 = json.contents.translated;
+        outputRoman.innerHTML=translatedText2; //output
+    }).catch(errorHandler)
 }
 
 btn.addEventListener("click", clickHandler);
